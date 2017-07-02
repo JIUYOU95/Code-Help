d617440cc44ef023828070f478d8b61c2b9c351e
刘隆检 <jian34long@163.com>
2017-07-02 21:30:57 +0800

    添加字体图标

--- APP/Admin/Controller/FrontController.class.php
+++ APP/Admin/Controller/FrontController.class.php
@@ -2,6 +2,7 @@
 namespace Admin\Controller;
 use Common\Controller\AuthController;
 use Admin\Common\Opadmin;
+use Common\Common\Category;
 class FrontController extends AuthController {
 	public function index(){
 		$this->display();
@@ -11,10 +12,27 @@ class FrontController extends AuthController {
 	 * 图标字体
 	 */
 	public function font(){
-		$data=D('Type')->getTreeData('tree','id','图标字体');
-		p($data);die;
-		$this->assign('data',$data);
+		$data=D('Type')->select();
+		$font=Category::unlimitedForLevel($data,'&nbsp;&nbsp;&nbsp;&nbsp;├─','1');
+		$list=Category::unlimitedForLayer($data,'child','1');
+		//p($list);die;
+		$this->assign('data',$font);
+		$this->assign('lists',$list);
 		$this->display();
+	}
+	/*
+	 * 新增图标字体
+	 */
+	public function add_font(){
+		$data=I('post.');
+		unset($data['id']);
+		$result=D('Font')->addData($data);
+		if ($result) {
+			A('Config')->add_log('添加图标字体-'.I('name'));
+			$this->success('添加成功',U('Admin/Front/font'));
+		}else{
+			$this->error('添加失败');
+		}
 	}
 
 }
