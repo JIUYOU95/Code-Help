f68479399d63461f2b275d67bd6d8ddb23400e5a
刘隆检 <jian34long@163.com>
2017-07-03 17:50:51 +0800

    新增字体图标

--- APP/Admin/Controller/FrontController.class.php
+++ APP/Admin/Controller/FrontController.class.php
@@ -12,10 +12,9 @@ class FrontController extends AuthController {
 	 * 图标字体
 	 */
 	public function font(){
-		$data=D('Type')->select();
+		$data=D('Type')->relation(true)->select();
 		$font=Category::unlimitedForLevel($data,'&nbsp;&nbsp;&nbsp;&nbsp;├─','1');
 		$list=Category::unlimitedForLayer($data,'child','1');
-		//p($list);die;
 		$this->assign('data',$font);
 		$this->assign('lists',$list);
 		$this->display();
@@ -28,10 +27,26 @@ class FrontController extends AuthController {
 		unset($data['id']);
 		$result=D('Font')->addData($data);
 		if ($result) {
-			A('Config')->add_log('添加图标字体-'.I('name'));
+			A('Config')->add_log('添加图标字体-'.I('zh_name'));
 			$this->success('添加成功',U('Admin/Front/font'));
 		}else{
 			$this->error('添加失败');
+		}
+	}
+	/**
+	 * 修改菜单
+	 */
+	public function edit_font(){
+		$data=I('post.');
+		$map=array(
+			'id'=>$data['id']
+			);
+		$result=D('Font')->editData($map,$data);
+		if ($result) {
+			A('Config')->add_log('修改图标字体-'.I('zh_name'));
+			$this->success('修改成功',U('Admin/Front/font'));
+		}else{
+			$this->error('修改失败');
 		}
 	}
 
--- APP/Admin/Model/TypeModel.class.php
+++ APP/Admin/Model/TypeModel.class.php
@@ -1,7 +1,16 @@
 <?php
 namespace Admin\Model;
-use Common\Model\BaseModel;
+use Think\Model\RelationModel;
 use Admin\Common\Opadmin;
-class TypeModel extends BaseModel {
+class TypeModel extends RelationModel {
+	protected $_link=array(
+		'Font'=> array(  
+	    	  'mapping_type'=>self::HAS_MANY,
+	          'class_name'=>'Font',
+	          'foreign_key'=>'pid',
+	          'mapping_name'=>'type',
+	          'mapping_fields'=>'id,zh_name,en_name',
+	    ),
+	);
 
 }
