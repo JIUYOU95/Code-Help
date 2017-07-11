51825d95f9e77199ff458d27aefadf2469de6491
刘隆检 <jian34long@163.com>
2017-07-11 09:38:25 +0800

    版本更新

---
+++ V1.0.2/APP/Admin/Controller/FrontController.class.php
@@ -0,0 +1,110 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\AuthController;
+use Admin\Common\Opadmin;
+use Common\Common\Category;
+class FrontController extends AuthController {
+	public function index(){
+		$this->display();
+	}
+
+	/*
+	 * 图标字体
+	 */
+	public function font(){
+		//分类
+		$data=D('Type')->relation(true)->select();
+		$font=Category::unlimitedForLevel($data,'&nbsp;&nbsp;&nbsp;&nbsp;├─','1');
+		$this->assign('data',$font);
+		//查询条件
+		if(I('pid')){
+			$id=I('pid');
+			$cids=Category::getChildsId($data,$id);
+			$cids[]=$id;
+			$where=array('pid'=>array('IN',$cids));
+			$this->pid=I('pid');
+		}else{
+			$where['pid']='5';
+			$this->pid='5';
+		}
+		$list=D('Font')->where($where)->order('id asc')->select();
+		$this->type=D('Type')->where(array('id'=>I('pid')))->find();
+		$this->assign('lists',$list);
+		$this->display();
+	}
+	/*
+	 * 新增图标字体
+	 */
+	public function add_font(){
+		$data=I('post.');
+		unset($data['id']);
+		$result=D('Font')->addData($data);
+		if ($result) {
+			A('Config')->add_log('添加图标字体-'.I('zh_name'));
+			$this->redirect('Admin/Front/font',array('pid'=>I('pid')));
+		}else{
+			$this->error('添加失败');
+		}
+	}
+	/**
+	 * 修改菜单
+	 */
+	public function edit_font(){
+		$data=I('post.');
+		$map=array(
+			'id'=>$data['id']
+			);
+		$result=D('Font')->editData($map,$data);
+		$pid=D('Font')->where($map)->find();
+		if ($result) {
+			A('Config')->add_log('修改图标字体-'.I('zh_name'));
+			$this->redirect('Admin/Front/font',array('pid'=>$pid['pid']));
+		}else{
+			$this->error('修改失败');
+		}
+	}
+	/*
+	 * 删除字体图标
+	 */
+	public function del_font(){
+		$id=I('get.id');
+		$where['id']=$id;
+		$result=D('Font')->where($where)->delete();
+		if($result){
+			A('Config')->add_log('删除字体图标-'.I('name'));
+			$this->redirect('Admin/Front/font',array('pid'=>I('pid')));
+		}else{
+			$this->error('删除字体图标失败');
+		}
+	}
+
+	/*
+	 * 颜色
+	 */
+	public function color(){
+		$this->color=D('Color')->order('id')->select();
+		$this->display();
+	}
+
+	/*
+	 * 新增颜色
+	 */
+	public function add_color(){
+		$color=D('Color');
+		$data=I('post.');
+		unset($data['id']);
+		if($color->create()){
+			$result=$color->addData($data);
+			if ($result) {
+				A('Config')->add_log('添加颜色-'.I('zh_name'));
+				$this->redirect('Admin/Front/color');
+			}else{
+				$this->error('添加失败');
+			}
+		}else{
+			$this->error($color->getError());
+		}
+	}
+
+
+}
---
+++ V1.0.2/APP/Admin/Controller/IndexController.class.php
@@ -0,0 +1,43 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\AuthController;
+use Admin\Common\Opadmin;
+class IndexController extends AuthController {
+    public function index(){
+    	$Opadmin=new Opadmin();
+    	$this->nickname=$Opadmin->getNickname();
+    	$config=$Opadmin->config();
+        $this->assign('config',$config);
+    	// 分配菜单数据
+		$nav_data=D('Nav')->getTree('level','order_number,id');
+		$this->assign('data',$nav_data);
+        //p($nav_data);die;
+        $this->display();
+    }
+
+    public function Welcome(){
+       $sysdata['host']= isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '');    //访问网址
+        $sysdata['ip']=get_server_ip();                         //服务器IP
+        $sysdata['port']=$_SERVER["SERVER_PORT"];               //端口号  
+        $Agent=$_SERVER['HTTP_USER_AGENT'];
+        //以下两条获取服务器时间，中国大陆采用的是东八区的时间,设置时区写成Etc/GMT-8
+        date_default_timezone_set("Etc/GMT-8");
+        $sysdata['systemtime'] = date("Y-m-d H:i:s",time());    //服务器时间
+        $sysdata['sysversion']= PHP_VERSION;                    //PHP版本
+        $sysdata['mysqlinfo']=$this-> _mysql_version();         //Mysql版本
+        $sysdata['browser']=get_broswer();                      //浏览器
+        $sysdata['system']=get_os();                            //操作系统 
+
+        $this->assign('sysdata',$sysdata);
+    	$this->display();
+    }
+
+    protected function _model(){
+        return new \Think\Model();
+    }
+    private function _mysql_version(){
+        $Model = self::_model();
+        $version = $Model->query("select version() as ver");
+        return $version[0]['ver'];
+    }
+}
